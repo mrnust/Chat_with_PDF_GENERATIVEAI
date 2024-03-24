36b24a1c48177fc42c8b54be06f5ac012c491194
Muhammad Rayyan <103200802+mrnust@users.noreply.github.com>
2024-03-24 16:11:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import cassio
 from PyPDF2 import PdfReader
 
 # Initialize OpenAI
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "KEY"
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
 # Initialize Cassio with Astra DB credentials
-ASTRA_DB_APPLICATION_TOKEN = "AstraCS:fQXcRdfxLhjzHgZWHZdILOwu:94209d2df354fd1475a6e177d3c41565ef3eccbc6f88f150e630801a276c0d44"
-ASTRA_DB_ID = "a8db79b4-3f9c-4f78-ae32-0f0f8f43b866"
+ASTRA_DB_APPLICATION_TOKEN = "KEY"
+ASTRA_DB_ID = "KEY"
 cassio.init(token=ASTRA_DB_APPLICATION_TOKEN, database_id=ASTRA_DB_ID)
 
 # Initialize Cassandra Vector Store
@@ -83,71 +83,4 @@ if __name__ == "__main__":
 
 
 
-
-
-#   only displays   chat bot -----------------------------------------------------
-    
-
-# import streamlit as st
-# from langchain.vectorstores.cassandra import Cassandra
-# from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-# from langchain.llms import OpenAI
-# from langchain.embeddings import OpenAIEmbeddings
-# from langchain.text_splitter import CharacterTextSplitter
-# import cassio
-
-# # Initialize OpenAI
-# OPENAI_API_KEY = "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"
-# llm = OpenAI(openai_api_key=OPENAI_API_KEY)
-# embedding = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-
-# # Initialize Cassio with Astra DB credentials
-# ASTRA_DB_APPLICATION_TOKEN = "AstraCS:fQXcRdfxLhjzHgZWHZdILOwu:94209d2df354fd1475a6e177d3c41565ef3eccbc6f88f150e630801a276c0d44"
-# ASTRA_DB_ID = "a8db79b4-3f9c-4f78-ae32-0f0f8f43b866"
-# cassio.init(token=ASTRA_DB_APPLICATION_TOKEN, database_id=ASTRA_DB_ID)
-
-# # Initialize Cassandra Vector Store
-# astra_vector_store = Cassandra(
-#     embedding=embedding,
-#     table_name="qa_demo",  # Provide your Astra DB table name
-#     session=None,
-#     keyspace=None  # No need to specify, Astra DB manages the keyspace
-# )
-
-# # Function to ask question and get response
-# def ask_question(question):
-#     try:
-#         response = astra_vector_index.query(question, llm=llm).strip()
-#         return response
-#     except Exception as e:
-#         print("Error querying Astra DB:", e)
-#         return "Sorry, there was an error processing your request. Please try again later."
-
-# # Initialize Vector Store Index
-# astra_vector_index = VectorStoreIndexWrapper(vectorstore=astra_vector_store)
-
-# # Initialize text splitter
-# text_splitter = CharacterTextSplitter(
-#     separator="\n",
-#     chunk_size=800,
-#     chunk_overlap=200,
-#     length_function=len
-# )
-
-# # Streamlit UI
-# def main():
-#     st.title("PDF Q&A Chatbot")
-
-#     st.write("You can now start asking questions.")
-
-#     question = st.text_input("Enter your question:")
-#     if st.button("Ask"):
-#         if question:
-#             answer = ask_question(question)
-#             st.write("Answer:", answer)
-#         else:
-#             st.warning("Please enter a question.")
-    
-# if __name__ == "__main__":
-#     main()
 
